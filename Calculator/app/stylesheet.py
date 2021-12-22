c67e131b15febd66aa3d8ef8d6aba800cf3e5ca8
ErEmEy7 <90320507+ErEmEy7@users.noreply.github.com>
2021-12-22 22:07:12 +0500

    Add files via upload

---
+++ Calculator/app/stylesheet.py
@@ -0,0 +1,27 @@
+style = '''
+QWidget {
+    background-color: #A9A9A9;
+}        
+
+QPushButton {
+    background-color: #808080;
+    color: #FFFFFF;
+
+    min-width:  70px;
+    max-width:  320px;
+    min-height: 70px;
+    max-height: 70px;
+
+    border-radius: 20px;        
+    border-width: 1px;
+    border-color: #000000;
+    border-style: solid;
+}
+QPushButton:hover {
+    background-color: #696969;
+}
+QPushButton:pressed {
+    background-color: #C0C0C0;
+}    
+
+'''
